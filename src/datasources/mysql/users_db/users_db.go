0f74f297d4b19b6423f72033d5ce6437d98a4e56
Ayush Paudel <ayushpaudel20@gmail.com>
2021-09-20 16:36:31 +0545

    updated environment variables

--- src/datasources/mysql/users_db/users_db.go
+++ src/datasources/mysql/users_db/users_db.go
@@ -4,31 +4,38 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/ayush723/utils-go_bookstore/logger"
 	"github.com/go-sql-driver/mysql"
 )
 
-// const (
-// 	mysql_users_username = "ayush"
-// 	mysql_users_password = "envy"
-// 	mysql_users_host     = "127.0.0.1"
-// 	// postgres_users_port     = 3306
-// 	mysql_users_schema = "users_db"
-// )
+const (
+	mysql_users_username = "ayush"
+	mysql_users_password = "envy"
+	mysql_users_schema = "users_db"
+)
 
 var (
 	Client *sql.DB
-	// username = os.Getenv(mysql_users_username)
-	// password = os.Getenv(mysql_users_password)
-	// host     = os.Getenv(mysql_users_host)
-	// // port     = (os.Getenv(postgres_users_port))
-	// schema = os.Getenv(mysql_users_schema)
+	
 )
 
 func init() {
-	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8", "ayush", "envy", "127.0.0.1", "users_db")
-	// fmt.Println(dataSourceName)
+	//setting environment variables
+	os.Setenv("mysql_users_username",mysql_users_username)
+	os.Setenv("mysql_users_password",mysql_users_password)
+	os.Setenv("mysql_users_schema",mysql_users_schema)
+
+	//getting environment variables
+	username := os.Getenv("mysql_users_username")
+	password := os.Getenv("mysql_users_password")
+	host     := os.Getenv("mysql_users_host")
+	schema := os.Getenv("mysql_users_schema")
+
+
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8", username, password, host, schema)
+	fmt.Println(dataSourceName)
 	// dataSourceName := "postgres://ayush:envy@localhost/users_db"
 	var err error
 	Client, err = sql.Open("mysql", dataSourceName)
